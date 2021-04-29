f4511a1e608a9db976ab4c2c060bdb366fa8ea6d
umfy <tarabi98@gmail.com>
2021-04-29 18:01:29 +0200

    pretty-devices

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,55 +39,53 @@ exports.user_POST_add = [
 
   body('email').optional({ checkFalsy: true }).isEmail().normalizeEmail(),
 
+  body('login').custom((value) => {
+    return User.findOne({ login: value }).then((line) => {
+      if (line) {
+        return Promise.reject('Login: ' + value + ' jest już w użyciu')
+      }
+    })
+  }),
+  async function (req, res, next) {
+    const errors = validationResult(req)
 
-    body('login').custom((value) => {
-        return User.findOne({ login: value }).then((line) => {
-           if (line) {
-              return Promise.reject('Login: ' + value + ' jest już w użyciu')
-           }
-        })
-     }),
-    async function(req, res, next) {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-          res.render('user-add', {
-            title: 'Dodaj użytkownika',
-            errors: errors.array(),
-            permission: ['technik', 'specjalista', 'admin'],
-            position: [
-              'specjalista',
-              'mechanik',
-              'robotyk',
-              'automatyk',
-              'kierownik',
-            ],
-          })
-          return
-        }
-        else {
-            const salt = await bcrypt.genSalt(10);
-            const hashPassword = await bcrypt.hash(req.body.password, salt)
-            const user = new User({
-                login: req.body.login,
-                name: req.body.name,
-                surname: req.body.surname,
-                email: req.body.email,
-                isAvaible: true,
-                isEmployed: true,
-                permission: req.body.permission,
-                position: req.body.position,
-                password: hashPassword
-            })
-            user.save(function(err) {
-                if(err) {
-                    return next(err)
-                }
-                res.redirect('/api/uzytkownicy')
-            })
+    if (!errors.isEmpty()) {
+      res.render('user-add', {
+        title: 'Dodaj użytkownika',
+        errors: errors.array(),
+        permission: ['technik', 'specjalista', 'admin'],
+        position: [
+          'specjalista',
+          'mechanik',
+          'robotyk',
+          'automatyk',
+          'kierownik',
+        ],
+      })
+      return
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hashPassword = await bcrypt.hash(req.body.password, salt)
+      const user = new User({
+        login: req.body.login,
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        isAvaible: true,
+        isEmployed: true,
+        permission: req.body.permission,
+        position: req.body.position,
+        password: hashPassword,
+      })
+      user.save(function (err) {
+        if (err) {
+          return next(err)
         }
         res.redirect('/api/uzytkownicy')
-      }
+      })
+    }
+    res.redirect('/api/uzytkownicy')
+  },
 ]
 
 exports.user_GET_update = function (req, res) {
@@ -119,55 +117,68 @@ exports.user_POST_update = [
 
   body('surname')
     .trim()
-    .isLength({min: 1, max: 100})
+    .isLength({ min: 1, max: 100 })
     .escape()
     .withMessage('Podaj Nazwisko'),
 
-  body('email')
-    .normalizeEmail()
-    .isEmail(),
+  body('email').normalizeEmail().isEmail(),
 
-    async function(req, res, next) {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.render('user-list', {title: 'Użytkownik', errors: errors.array()})
-            return
+  async function (req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.render('user-list', { title: 'Użytkownik', errors: errors.array() })
+      return
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hashPassword = await bcrypt.hash(req.body.password, salt)
+      User.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        isAvaible: !!req.body.isAvaible,
+        isEmployed: !!req.body.isEmployed,
+        permission: req.body.permission,
+        position: req.body.position,
+        password: hashPassword,
+      }).exec(function (err, result) {
+        if (err) {
+          return next(err)
         }
-        else {
-            const salt = await bcrypt.genSalt(10);
-            const hashPassword = await bcrypt.hash(req.body.password, salt)
-            User.findByIdAndUpdate(req.params.id, {
-                name: req.body.name,
-                surname: req.body.surname,
-                email: req.body.email,
-                isAvaible: !!req.body.isAvaible,
-                isEmployed: !!req.body.isEmployed,
-                permission: req.body.permission,
-                position: req.body.position,
-                password: hashPassword
-            })
-            .exec(function(err, result) {
-                if(err) {
-                    return next(err)
-                }
-                res.redirect('/api/uzytkownicy/' + req.params.id)
-            })
-        }
+        res.redirect('/api/uzytkownicy/' + req.params.id)
+      })
+    }
+  },
+]
 
-    }];
-
-exports.user_GET_delete = function (req, res) {
-  User.findByIdAndRemove(req.params.id).exec(function (err, result) {
+exports.user_GET_delete = function (req, res, next) {
+  User.findById(req.params.id).exec(function (err, result) {
     if (err) {
       return next(err)
     }
-    res.redirect('/api/uzytkownicy/')
+    if (result == null) {
+      // No results.
+      res.redirect('/api/uzytkownicy/')
+    }
+    res.render('user-delete', {
+      title: 'Usuń Użytkownika',
+      user: result,
+    })
+  })
+}
+
+// Handle device delete on POST.
+exports.user_POST_delete = function (req, res, next) {
+  User.findByIdAndRemove(req.body.userid, function (err) {
+    if (err) {
+      return next(err)
+    }
+    // Success - go to author list
+    res.redirect('/api/uzytkownicy')
   })
 }
 
 exports.user_GET_one = function (req, res, next) {
-  User.findById(req.params.id)
-    .exec(function (err, result) {
+  User.findById(req.params.id).exec(function (err, result) {
     if (err) {
       return next(err)
     }
@@ -198,52 +209,54 @@ exports.user_GET_shift = function (req, res, next) {
     })
 }
 
-exports.user_POST_shift = function(req, res) {
-    var changeList = req.body
-    var shift = req.body.shift
+exports.user_POST_shift = function (req, res) {
+  var changeList = req.body
+  var shift = req.body.shift
 
-    User.updateMany({shift: shift}, {shift: 0})
-    .exec(function(err, result) {
-        if(err) {
-            res.status(500).json({error: err})
+  User.updateMany({ shift: shift }, { shift: 0 }).exec(function (err, result) {
+    if (err) {
+      res.status(500).json({ error: err })
+    }
+  })
+
+  for (const [key, value] of Object.entries(changeList)) {
+    if (value != shift) {
+      User.updateOne(
+        { _id: value },
+        {
+          $set: { shift: shift },
         }
-    })
-    
-    for(const [key, value] of Object.entries(changeList)) {
-        if(value != shift) {
-            User.updateOne({_id: value}, {
-                $set: {shift: shift}
-            })
-            .exec(function(err,result) {
-                if(err) {
-                    res.status(500).json({error: err})
-                }
-            })
-            }
+      ).exec(function (err, result) {
+        if (err) {
+          res.status(500).json({ error: err })
         }
-        res.status(200).json({body: 'Zapisano pomyślnie zmianę '})
+      })
+    }
+  }
+  res.status(200).json({ body: 'Zapisano pomyślnie zmianę ' })
 }
 
 /// LOGIN MANAGMENT
-exports.user_GET_login = function(req, res) {
-    res.render('login', {title: 'System Raportowania UR Spawalnia'})
+exports.user_GET_login = function (req, res) {
+  res.render('login', { title: 'System Raportowania UR Spawalnia' })
 }
-exports.user_POST_login = async function(req, res) {
-    console.log(req.body.password)
-    User.findOne({login: req.body.login})
-    .exec(async function(err, result) {
-        if(err) {
-            return next(err)
-        }
-        if(!result) {
-            console.log('brak loginu')
-            res.status(400).render('login', {errs:'Podany login nie widnieje w bazie danych'})
-        }
-        const validPass = await bcrypt.compare(req.body.password, result.password)
-        if(!validPass) {
-            console.log('złe hasło')
-            res.status(400).render('login',{errs:'Złe hasło!'})
-        }
-        res.status(200).render()
-    })
+exports.user_POST_login = async function (req, res) {
+  console.log(req.body.password)
+  User.findOne({ login: req.body.login }).exec(async function (err, result) {
+    if (err) {
+      return next(err)
+    }
+    if (!result) {
+      console.log('brak loginu')
+      res
+        .status(400)
+        .render('login', { errs: 'Podany login nie widnieje w bazie danych' })
+    }
+    const validPass = await bcrypt.compare(req.body.password, result.password)
+    if (!validPass) {
+      console.log('złe hasło')
+      res.status(400).render('login', { errs: 'Złe hasło!' })
+    }
+    res.status(200).render()
+  })
 }
